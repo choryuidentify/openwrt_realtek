f6e5404ccd1d27ac525728365f387277fa1461d1
Dimazhan <dimazhan@list.ru>
2020-11-27 12:39:03 +0700

    switch to 5.4

--- target/linux/realtek/rtl8197d/target.mk
+++ target/linux/realtek/rtl8197d/target.mk
@@ -6,7 +6,6 @@ SUBTARGET:=rtl8197d
 BOARDNAME:=RTL8197d based boards
 ARCH_PACKAGES:=realtek_lx53
 CPU_TYPE:=lx53
-KERNEL_PATCHVER:=4.14
 
 define Target/Description
         Build firmware images for Realtek RTL8197D based boards.
