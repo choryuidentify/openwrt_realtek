b45c05116f2c612052d433ecd0160eb496d96a93
pine <epiniguin@gmail.com>
2021-05-15 12:38:52 +0300

    Revert "switch to 5.4"
    This reverts commit f6e5404ccd1d27ac525728365f387277fa1461d1.

--- target/linux/realtek/rtl8196e/target.mk
+++ target/linux/realtek/rtl8196e/target.mk
@@ -6,6 +6,7 @@ SUBTARGET:=rtl8196e
 BOARDNAME:=RTL8196e based boards
 ARCH_PACKAGES:=realtek_lx43
 CPU_TYPE:=lx43
+KERNEL_PATCHVER:=4.14
 
 define Target/Description
         Build firmware images for Realtek RTL8196E based boards.
--- target/linux/realtek/rtl8197f/target.mk
+++ target/linux/realtek/rtl8197f/target.mk
@@ -6,6 +6,7 @@ ARCH:=mipsel
 SUBTARGET:=rtl8197f
 BOARDNAME:=RTL8197f based boards
 CPU_TYPE:=24kc
+KERNEL_PATCHVER:=4.14
 
 define Target/Description
         Build firmware images for Realtek RTL8197F based boards.
